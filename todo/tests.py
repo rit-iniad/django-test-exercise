4ff4a0c377db5465611c139b23ba2a46911f4dce
1F10220085 <s1f102200851@iniad.org>
2023-07-06 13:24:17 +0900

    修正2

--- todo/tests.py
+++ todo/tests.py
@@ -9,6 +9,7 @@ class SampleTestCase(TestCase):
     def test_sample1(self):
         self.assertEqual(1 + 2, 3)
 
+
 class TaskModelTestCase(TestCase):
     def test_create_task1(self):
         due = timezone.make_aware(datetime(2023, 6, 30, 23, 59, 59))
@@ -20,7 +21,7 @@ class TaskModelTestCase(TestCase):
         self.assertFalse(task.completed)
         self.assertEqual(task.due_at, due)
 
-    def test_create_task2 (self):
+    def test_create_task2(self):
         task = Task(title='task2')
         task.save()
 
@@ -52,6 +53,7 @@ class TaskModelTestCase(TestCase):
 
         self.assertFalse(task.is_overdue(current))
 
+
 class TodoViewTestCase(TestCase):
     def test_index_get(self):
         client = Client()
@@ -73,7 +75,7 @@ class TodoViewTestCase(TestCase):
     def test_index_get_order_post(self):
         task1 = Task(title='task1', due_at=timezone.make_aware(datetime(2023, 7, 1)))
         task1.save()
-        task2 = Task(title='task2', due_at=timezone.make_aware(datetime (2023, 8, 1)))
+        task2 = Task(title='task2', due_at=timezone.make_aware(datetime(2023, 8, 1)))
         task2.save()
         client = Client()
         response = client.get('/?order=post')
