d9aa05bef03f99875b9a4ef9096d8b7a742eae79
1F10220079 <s1F102200794@iniad.org>
2023-07-21 10:04:33 +0900

    大まかにコメント機能の追加

--- todo/tests.py
+++ todo/tests.py
@@ -111,5 +111,4 @@ class TodoViewTestCase(TestCase):
     def test_detail_get_fail(self):
         client = Client()
         response = client.get('/1/')
-
         self.assertEqual(response.status_code, 404)
